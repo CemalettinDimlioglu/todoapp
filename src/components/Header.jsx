eacfaa118ce96d37402d82a03a33280d2fc6c6aa
CMLTTN <cemaldim@gmail.com>
2023-03-13 16:25:30 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,14 @@ const Header = () => {
   return (
     <div>
       <h1 className="text-center text-danger m-5">Todo App </h1>
-      <InputGroup className="mb-3">
+      <InputGroup className="mb-3 w-50 d-flex mx-auto">
         <Form.Control
-          placeholder="Recipient's username"
+          placeholder="Enter New Todo..."
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
+          onChange={(e)=> console.log(e.target.value)}
         />
-        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+        <button className='input-group-text bg-success' id="basic-addon2">Add Todo</button>
       </InputGroup>
     </div>
   );
