0f429ef85a0fe1c3ef8b7d7d1eeabc7f7fe8fb9c
CMLTTN <cemaldim@gmail.com>
2023-03-13 17:40:44 +0300

    Finished

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import {useState} from "react";
 import Header from './components/Header';
 import TodoList from "./components/TodoList"
 
+const todoList = [
+  {
+    id: 1,
+    text: "React ile todoList uygulaması",
+    completed: false,
+  },
+  {
+    id: new Date().getTime(),
+    text: "uuid kullanımı",
+    completed: false,
+  },
+];
 
 function App() {
-  const [todos, setTodos]=useState([])
+  const [todos, setTodos]=useState(todoList)
 
   return (
     <div className="container">
       <Header setTodos={setTodos} todos={todos} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import React from "react";
+import { ListGroup } from "react-bootstrap";
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos, setTodos }) => {
+     const deleteTodo=(id)=>{
+          const newTodos = todos.filter(todo => todo.id !==id)
+          setTodos(newTodos)
+
+     }
   return (
     <div>
       <h2 className="text-center text-secondary">Todos</h2>
-      <ListGroup className='w-50 d-flex mx-auto' >
-        <ListGroup.Item variant="success">Success</ListGroup.Item>
-        {
-          todos.map((todo)=>{ <ListGroup.Item variant="success">{todo.text}</ListGroup.Item>;})
-        }
+      <ListGroup className="w-50 d-flex mx-auto">
+        {/* <ListGroup.Item variant="success">Success</ListGroup.Item> */}
+        {todos.map((todo) => (
+          <ListGroup.Item
+            variant="success"
+            className="m-2 text-capitalize rounded-5 d-flex justify-content-between"
+          >
+            <span>{todo.text}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="red"
+              role= "button"
+              onClick={()=>deleteTodo(todo.id)}
+              className="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg>
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </div>
   );
-}
+};
 
-export default TodoList
+export default TodoList;
